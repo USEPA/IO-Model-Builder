f218b6de980eebcfc995116e196d71530d0fa7d0
msrocka <michael.srocka@gmail.com>
2016-04-12 10:37:49 +0200

    restructured test data

--- tests/test_economic.py
+++ tests/test_economic.py
@@ -4,10 +4,13 @@ import unittest
 
 class TestEconomicModule(unittest.TestCase):
 
+    def setUp(self):
+        self.df = io.DataFolder('./sample_data')
+        self.em = self.df.get_economic_module()
+
     def test_files_present(self):
-        folder = io.DataFolder('../data')
-        self.assertTrue(folder.has_file(io.USE_TABLE))
-        self.assertTrue(folder.has_file(io.MAKE_TABLE))
+        self.assertTrue(self.df.has_file(io.USE_TABLE))
+        self.assertTrue(self.df.has_file(io.MAKE_TABLE))
 
 if __name__ == '__main__':
     unittest.main()
