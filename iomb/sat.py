4891181447be9069e1c31f59e9504d05fc0b56af
msrocka <michael.srocka@gmail.com>
2016-05-19 17:16:09 +0200

    function for converting satellite tables to data frames

--- iomb/sat.py
+++ iomb/sat.py
@@ -1,22 +1,33 @@
 import iomb.util as util
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 
 class Flow(object):
     def __init__(self, name='', category='', sub_category='', unit=''):
         self.name = name
         self.category = category
-        self.subCategory = sub_category
+        self.sub_category = sub_category
         self.unit = unit
-        self.id = util.uuid_of_flow(name, category, sub_category, unit)
+        self.uid = util.uuid_of_flow(name, category, sub_category, unit)
+
+    @property
+    def key(self):
+        return '%s / %s / %s [%s]' % (self.category, self.sub_category,
+                                      self.name, self.unit)
 
 
 class Sector(object):
-    def __init__(self, name='', code='', location=''):
+    def __init__(self, name='', code='', location='US'):
         self.name = name
         self.code = code
         self.location = location
-        self.id = util.uuid_of_process(name, code, location)
+        self.uid = util.uuid_of_process(name, code, location)
+
+    @property
+    def key(self):
+        return '%s - %s - %s' % (self.code, self.name, self.location)
 
 
 class Table(object):
@@ -43,21 +54,31 @@ class Table(object):
                     category=row[2],
                     sub_category=row[3],
                     unit=row[9])
-        if flow.id not in self.flow_idx:
+        if flow.uid not in self.flow_idx:
             i = len(self.flows)
             self.flows.append(flow)
-            self.flow_idx[flow.id] = i
-        return self.flow_idx[flow.id]
+            self.flow_idx[flow.uid] = i
+        return self.flow_idx[flow.uid]
 
     def _read_sector(self, row) -> int:
         sector = Sector(name=row[4],
                         code=row[5],
                         location=row[6])
-        if sector.id not in self.sector_idx:
+        if sector.uid not in self.sector_idx:
             j = len(self.sectors)
             self.sectors.append(sector)
-            self.sector_idx[sector.id] = j
-        return self.sector_idx[sector.id]
+            self.sector_idx[sector.uid] = j
+        return self.sector_idx[sector.uid]
+
+    def as_data_frame(self) -> pd.DataFrame:
+        rows, cols = len(self.flows), len(self.sectors)
+        data = np.zeros((rows, cols), dtype=np.float64)
+        for i, row in self.entries.items():
+            for j, val in row.items():
+                data[i, j] = val
+        index = [f.key for f in self.flows]
+        columns = [s.key for s in self.sectors]
+        return pd.DataFrame(data=data, index=index, columns=columns)
 
     def viz_flow(self, name):
         idx = None
