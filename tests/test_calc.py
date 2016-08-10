17925e86ec703b56e6b4c0b18f007e2dd81f225d
msrocka <michael.srocka@gmail.com>
2016-08-10 16:25:27 +0200

    calculation tests with perspective checks

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -53,6 +53,10 @@ class TestCalc(unittest.TestCase):
         self.model = iomb.make_model(drc, [sat], sectors, [lcia])
         self.demand = {'3/car assembly/us': 1}
 
+        self.co2 = 'simpleeconomymethod/carbon dioxide emissions/kgco2e'
+        self.so2 = 'simpleeconomymethod/sulfur dioxide emissions/kgso2e'
+        self.water = 'simpleeconomymethod/water use/kgh2oe'
+
     def tearDown(self):
         for path in self.paths:
             os.remove(path)
@@ -63,18 +67,53 @@ class TestCalc(unittest.TestCase):
 
     def test_direct_perspective(self):
         r = calc.calculate(self.model, self.demand, calc.DIRECT_PERSPECTIVE)
-        print(r.lci_total)
-        print(r.lcia_total)
-        print(r.lcia_contributions)
+        self._check_totals(r)
+        flows = [self.co2, self.so2, self.water]
+        expected = {
+            '1/electricity/us': [0.617283951, 0.041152263, 1.028806584],
+            '2/steel parts/us': [0.49382716, 0.024691358, 0.49382716],
+            '3/car assembly/us': [0, 0, 0]
+        }
+        c = r.lcia_contributions
+        for sector, values in expected.items():
+            for i in range(0, len(flows)):
+                self.assertAlmostEqual(c.ix[flows[i], sector], values[i])
 
     def test_intermediate_perspective(self):
         r = calc.calculate(self.model, self.demand,
                            calc.INTERMEDIATE_PERSPECTIVE)
-        print(r.lcia_contributions)
+        self._check_totals(r)
+        flows = [self.co2, self.so2, self.water]
+        expected = {
+            '1/electricity/us': [0.685871056, 0.045724737, 1.143118427],
+            '2/steel parts/us': [0.823045267, 0.045724737, 1.005944216],
+            '3/car assembly/us': [1.234567901, 0.073159579, 1.691815272]
+        }
+        c = r.lcia_contributions
+        for sector, values in expected.items():
+            for i in range(0, len(flows)):
+                self.assertAlmostEqual(c.ix[flows[i], sector], values[i])
 
     def test_final_perspective(self):
         r = calc.calculate(self.model, self.demand, calc.FINAL_PERSPECTIVE)
-        print(r.lcia_contributions)
+        self._check_totals(r)
+        flows = [self.co2, self.so2, self.water]
+        expected = {
+            '1/electricity/us': [0, 0, 0],
+            '2/steel parts/us': [0, 0, 0],
+            '3/car assembly/us': [1.111111111, 0.065843621, 1.522633745]
+        }
+        c = r.lcia_contributions
+        for sector, values in expected.items():
+            for i in range(0, len(flows)):
+                self.assertAlmostEqual(c.ix[flows[i], sector], values[i])
+
+    def _check_totals(self, result: calc.Result):
+        total = result.lcia_total
+        self.assertAlmostEqual(total.ix[self.co2, 'Total'], 1.111111111)
+        self.assertAlmostEqual(total.ix[self.so2, 'Total'], 0.065843621)
+        self.assertAlmostEqual(total.ix[self.water, 'Total'], 1.522633745)
+
 
 if __name__ == '__main__':
     unittest.main()
