1b1f8e80c82f2a65d2fb04f6ec27e9581e63a683
msrocka <michael.srocka@gmail.com>
2017-05-31 11:59:53 +0200

    added export of pre-calculated matrices

--- iomb/dqi.py
+++ iomb/dqi.py
@@ -177,6 +177,22 @@ class Matrix(object):
             return m
 
     @staticmethod
+    def from_sat_table(sat_table):
+        m = Matrix(len(sat_table.flows), len(sat_table.sectors))
+        for row in range(0, m.rows):
+            for col in range(0, m.cols):
+                flow = sat_table.flows[row]
+                sector = sat_table.sectors[col]
+                e = sat_table.get_entry(flow.key, sector.key)
+                if e is None or not isinstance(e.data_quality_entry, str):
+                    m[row, col] = None
+                    continue
+                sat_dq = e.data_quality_entry.strip()[1: len(dq_entry) - 1]
+                # TODO currently only support for plain integer values
+                m[row, col] = [int(i) for i in sat_dq.split(';')]
+        return m
+
+    @staticmethod
     def rand(rows: int, cols: int, tsize=5, mini=1, maxi=5):
         m = Matrix(rows, cols)
         for row in range(0, rows):
---
+++ iomb/matio.py
@@ -0,0 +1,116 @@
+import csv
+import os
+import struct
+
+import iomb
+
+
+class Export(object):
+
+    def __init__(self, model):
+        self.model = model
+        self.folder = 'data'
+
+    def to_dir(self, folder: str):
+        """ Exports the matrices of the model to the given folder.
+
+            Args:
+                folder (str): the path to the export folder
+        """
+        self.folder = folder
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        # TODO: currently only supports models with LCIA methods
+
+        # the direct requirements matrix: A
+        A = model.drc_matrix
+        self.__write_matrix(A.values, 'A')
+
+        # the Leontief inverse: L
+        L = iomb.calc.leontief_inverse(A)
+        self.__write_matrix(L.values, 'L')
+
+        # the satellite matrix: B
+        B = model.sat_table.as_data_frame().reindex(
+            columns=A.index, fill_value=0.0)
+        self.__write_matrix(B.values, 'B')
+
+        # the characterization factors: C
+        C = model.ia_table.as_data_frame().reindex(
+            columns=B.index, fill_value=0.0)
+        self.__write_matrix(C.values, 'C')
+
+        # the direct impacts per 1 USD sector output: D
+        D = C.values @ B.values
+        self.__write_matrix(D, 'D')
+
+        # the upstream impacts per 1 USD sector output: U
+        U = D @ L.values
+        self.__write_matrix(U, 'U')
+
+        # the data quality matrix of the satellite table: B_dqi
+        B_dqi = iomb.dqi.Matrix.from_sat_table(model.sat_table)
+        B_dqi.to_csv('%/B_dqi.csv' % self.folder)
+
+        # the data quality matrix of the direct impacts: D_dqi
+        D_dqi = B_dqi.aggregate_mmult(C.values, B.values, left=False)
+        D_dqi.to_csv('%/D_dqi.csv')
+
+        # the data quality matrix of the upstream impacts: U_dqi
+        U_dqi = D_dqi.aggregate_mmult(D, L.values, left=True)
+        U_dqi.to_csv('%/U_dqi.csv')
+
+        # write matrix indices with meta-data
+        self.__write_sectors(A)
+        self.__write_flows(B)
+        self.__write_indicators(C)
+
+    def __write_matrix(self, M, name: str):
+        path = '%s/%s.bin' % (self.folder, name)
+        with open(path, 'wb') as f:
+            rows, cols = M.shape
+            f.write(struct.pack("<i", rows))
+            f.write(struct.pack("<i", cols))
+            for col in range(0, cols):
+                for row in range(0, rows):
+                    val = m[row, col]
+                    f.write(struct.pack("<d", val))
+
+    def __write_sectors(self, A):
+        path = '%s/sectors.csv' % self.folder
+        with open(path, 'w', encoding='utf-8', newline='\n') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Index', 'ID', 'Name', 'Code', 'Location'])
+            i = 0
+            for sector_key in A.index:
+                sector = self.model.sectors.get(sector_key)
+                writer.writerow([i, sector_key, sector.name, sector.code,
+                                 sector.location])
+                i += 1
+
+    def __write_flows(self, B):
+        path = '%s/flows.csv' % self.folder
+        with open(path, 'w', encoding='utf-8', newline='\n') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Index', 'ID', 'Name', 'Category', 'Sub-Category',
+                             'Unit', 'UUID'])
+            i = 0
+            for flow_key in B.index:
+                flow = self.model.sat_table.get_flow(flow_key)
+                writer.writerow([i, flow.key, flow.name, flow.category,
+                                 flow.sub_category, flow.unit, flow.uid])
+                i += 1
+
+    def __write_indicators(self, C):
+        path = '%s/indicators.csv' % self.folder
+        with open(path, 'w', encoding='utf-8', newline='\n') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Index', 'ID', 'Name', 'Code', 'Unit', 'UUID'])
+            i = 0
+            for cat_key in C.index:
+                idx = self.model.ia_table.category_idx.get(cat_key)
+                cat = self.model.ia_table.categories[idx]
+                writer.writerow([i, cat_key, cat.name, '',
+                                 cat.ref_unit, cat.uid])
+                i += 1
